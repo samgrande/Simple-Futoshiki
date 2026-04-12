df9131af4b8c96a66e47335ee4fedbfbf3232b27
soun059 <soujanna2015newera@gmail.com>
2026-04-12 15:06:39 +0530

    Fix release build: add proguard rules and clean up Gradle config
    - Add missing proguard-rules.pro (was crashing minifyReleaseWithR8)
    - Migrate kotlinOptions to kotlin.compilerOptions DSL (deprecation fix)
    - Remove deprecated gradle.properties entries that have become AGP 9.x defaults
    - Retain android.builtInKotlin=false and android.newDsl=false to work around
      Kotlin plugin 2.2.x / AGP 9.x new-DSL incompatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,13 +37,16 @@ android {
             }
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 }
 
